b009c8a5338fa5ac3079b98b6f370444a028d9ee
Mark Glants <m@gearage.ru>
2020-04-17 04:49:45 +0300

    Add twitch registration

--- registration.py
+++ registration.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 import asyncio, random, os, datetime, platform
@@ -35,8 +36,27 @@ def start_driver():
     driver = webdriver.Chrome(executable_path=driver_dir, options=chrome_options)
     driver.implicitly_wait(10)
     return driver
+def reg_twitch():
+    dd = random.randint(1, 28)
+    mm = random.randint(1, 12)
+    yyyy = random.randint(1980,2000)
+    username = 'KEKCHEBURECK123'
+    password = 'Zaq_12345Zaq_12345'
+    driver = start_driver()
+    actions = ActionChains(driver)
+    driver.get('https://www.twitch.tv/signup')
+    driver.find_element_by_xpath('//*[@id="signup-username"]').send_keys(username)
+    driver.find_element_by_xpath('//*[@id="password-input"]').send_keys(password)
+    driver.find_element_by_xpath('//*[@id="password-input-confirmation"]').send_keys(password)
+    select = Select(driver.find_element_by_tag_name('select'))
+    select.select_by_index(mm)
+    actions.send_keys(Keys.TAB,dd, Keys.TAB, yyyy,Keys.TAB,'test@gearage.ru')
+    actions.perform()
+    sleep(random.uniform(9.0,15.0))
+    driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div[3]/div/div/div/div[3]/form/div/div[5]/button').click()
+    return driver
 
-if __name__ == "__main__":
+def reg_rito():
     dd = random.randint(1, 28)
     mm = random.randint(1, 12)
     yyyy = random.randint(1980,2000)
@@ -56,4 +76,8 @@ if __name__ == "__main__":
     driver.find_element_by_tag_name('input').click()
     actions.send_keys(Keys.ENTER)
     actions.perform()
+    return driver
+
+if __name__ == "__main__":
+        reg_twitch()
 
