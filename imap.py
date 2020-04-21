461a95ce4a50e8ab59bfbd89d567b41e84ad4e3f
Mark Glants <m@gearage.ru>
2020-04-22 02:14:13 +0300

    ADD documentation, create function in function

--- imap.py
+++ imap.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python3
+#Function can recieve codes and links from mail server for verification
+#Example  USAGE
+#get_imap('<IMAP_SERVER>', '<IMAP_LOGIN>'', '<IMAP_PASSWORD>', '1vallie@gearage.ru', whattodo)
+#print (get_imap('mail.ur30.ru', 'vallie@gearage.ru', 'jackwolf', '1vallie@gearage.ru', twitch_login_verify))
+
+#WHATTODO
+#1. twitch_reg_verify
+#2. twitch_login_verify
+#3. TBD
+
+
 import imaplib
 import pprint
 import ssl
 import email
 import re
-imap_login = 'vallie@gearage.ru'
-imap_pass = 'jackwolf'
-reg_email = '1vallie@gearage.ru'
-from_email = "Twitch"
-# Load system's trusted SSL certificates
-tls_context = ssl.create_default_context()
-# Connect (unencrypted at first)
-server = imaplib.IMAP4('mail.ur30.ru')
-# Start TLS encryption. Will fail if TLS session can't be established
-server.starttls(ssl_context=tls_context)
-# Login. ONLY DO THIS AFTER server.starttls() !!
-server.login(imap_login, imap_pass)
-#Выбираем папку IMAP
-server.select('INBOX', readonly=True)
+twitch_from_email = 'Twitch' #Twitch Sender name
+riot_from_email = '' # RION Sender name
+imap_folder = 'INBOX' # default for all mail servers
 
 
-def twitch_reg_verify():
+def get_imap(imap_server, imap_login, imap_pass, reg_email, whattodo):
+
+    # Load system's trusted SSL certificates
+    tls_context = ssl.create_default_context()
+    # Connect (unencrypted at first)
+    server = imaplib.IMAP4(imap_server)
+    # Start TLS encryption. Will fail if TLS session can't be established
+    server.starttls(ssl_context=tls_context)
+    # Login. ONLY DO THIS AFTER server.starttls() !!
+    server.login(imap_login, imap_pass)
+    #Выбираем папку IMAP
+    server.select(imap_folder, readonly=True)
+
+    return whattodo(server,reg_email) # Вызываем функцию и что делать дальше.
+
+def twitch_reg_verify(server,reg_email):
     try:
         #Ищем конкретные письма
-        typ, data = server.uid ('search', None, 'TO {} FROM {} TEXT "Please verify your Twitch account."'.format(reg_email,from_email))
+        typ, data = server.uid ('search', None, 'TO {} FROM {} TEXT "Please verify your Twitch account."'.format(reg_email,twitch_from_email))
         latest_email_uid = data[0].split()[-1] # Получаем сроку номеров писем , Разделяем ID писем
         result, data = server.uid('fetch', latest_email_uid, '(RFC822)') # Получаем  письмо по rfc822
         raw_email = data[0][1].decode('utf-8') # вовзрашаем  письмо в UTF-8
@@ -38,10 +53,11 @@ def twitch_reg_verify():
             pass
 
     return code
-def twitch_login_verify():
+    
+def twitch_login_verify(server,reg_email):
     try:
         #Ищем конкретные письма
-        typ, data = server.uid ('search', None, 'TO {} FROM {} TEXT "Please enter the following code."'.format(reg_email,from_email))
+        typ, data = server.uid ('search', None, 'TO {} FROM {} TEXT "Please enter the following code."'.format(reg_email,twitch_from_email))
         latest_email_uid = data[0].split()[-1] # Получаем сроку номеров писем , Разделяем ID писем
         result, data = server.uid('fetch', latest_email_uid, '(RFC822)') # Получаем  письмо по rfc822
         raw_email = data[0][1].decode('utf-8') # вовзрашаем  письмо в UTF-8
@@ -53,33 +69,3 @@ def twitch_login_verify():
         except:
             pass
     return code
-
-    #return code
-print (twitch_login_verify())
-# try:
-#     #Выбираем папку IMAP
-#     server.select('INBOX', readonly=True)
-#     #Ищем конкретные письма
-#     typ, data = server.uid ('search', None, 'TO {} FROM {} TEXT "Please verify your Twitch account."'.format(reg_email,from_email))
-#     latest_email_uid = data[0].split()[-1] # Получаем сроку номеров писем , Разделяем ID писем
-#     result, data = server.uid('fetch', latest_email_uid, '(RFC822)') # Получаем  письмо по rfc822
-#     raw_email = data[0][1].decode('utf-8') # вовзрашаем  письмо в UTF-8
-#     email_message = email.message_from_string(raw_email) # Засовывем писмо в либу для машинного чтения
-#     subject = (email_message['Subject']) #Twitch содержит код в Теме письма
-#     code_regex = re.findall(r'\d{6}', subject) # Проверяем subject на наш regexp
-#     code = code_regex[0] # Получаем код внутри найденого regex
-#     print("На нахуй код тебе от твича " + code)
-# finally:
-#     try:
-#         server.close()
-#     except:
-#         pass
-#     server.logout()
-
-# Select mailbox
-#server.select("INBOX")
-#typ, msg_ids = server.search (
-#           None, 'UNSEEN HEADER FROM "twitch.tv"'
-#)
-#print(msg_ids)
-#print(msg_ids)
